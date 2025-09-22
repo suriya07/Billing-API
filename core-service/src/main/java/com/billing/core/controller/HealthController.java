bffd458e9fc8d6eeab86fba4900edafdbb255651
Suriya Prakash <suriya227@gmail.com>
2025-09-22 23:28:42 +0530

    Added health File

---
+++ core-service/src/main/java/com/billing/core/controller/HealthController.java
@@ -0,0 +1,14 @@
+package com.billing.core.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HealthController {
+    @GetMapping("/health")
+    public String health() {
+        return "Core Service is up and running!";
+    }
+}
